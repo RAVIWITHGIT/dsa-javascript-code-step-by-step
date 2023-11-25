0d7ddc4518b1fbc3705a30834e918229be080d26
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-25 17:48:29 +0530

    Merge Two Array

---
+++ tut6/main.js
@@ -0,0 +1,34 @@
+// ------------------------- MERGE TWO ARRAY ------------------------------------
+// let fArray = [22,33,66,77,89]
+// let sArray = [66,90,21,67,76,37]
+
+// let nArray = []
+// for (let i = 0; i < fArray.length; i++) {
+//     nArray[i] = fArray[i]
+    
+// }
+// for (let j = 0; j < sArray.length; j++) {
+//     nArray[fArray.length+j] = sArray[j]
+    
+// }
+
+// console.log(nArray)
+
+
+// ------------------------- merge two array using built in array
+// let fArray = [22,33,66,77,89]
+// let sArray = [66,90,21,67,76,37]
+// let nArray = [...fArray,...sArray]
+// console.log(nArray)
+
+// let fArray = [22,33,66,77,89]
+// console.log(...fArray)
+// console.log([...fArray])
+
+
+
+// ----------------------- reverse array using built in function
+// let fArray = [22,33,66,77,89]
+// console.log(fArray.reverse())
+
+
