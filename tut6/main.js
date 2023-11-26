65bca6b8e3e617b75f94ad9b602083f858aceec1
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-26 15:26:23 +0530

    update tut6

--- tut6/main.js
+++ tut6/main.js
@@ -5,11 +5,11 @@
 // let nArray = []
 // for (let i = 0; i < fArray.length; i++) {
 //     nArray[i] = fArray[i]
-    
+
 // }
 // for (let j = 0; j < sArray.length; j++) {
 //     nArray[fArray.length+j] = sArray[j]
-    
+
 // }
 
 // console.log(nArray)
@@ -32,3 +32,39 @@
 // console.log(fArray.reverse())
 
 
+// ------------------------- MERGE TWO ARRAY and sort new array
+
+// ----- merge two array
+let fArray = [22,33,66,77,89]
+let sArray = [66,90,21,67,76,37]
+
+let nArray = []
+for (let i = 0; i < fArray.length; i++) {
+    nArray[i] = fArray[i]
+
+}
+for (let j = 0; j < sArray.length; j++) {
+    nArray[fArray.length+j] = sArray[j]
+
+}
+
+// ---- sort manually
+
+
+for (let i = 0; i < nArray.length; i++) {
+    for (let j = 0; j < nArray.length-1-i; j++) {
+        let temp = 0
+        if(nArray[j]<nArray[j+1]){
+    
+        }
+        else{
+            temp = nArray[j] 
+            nArray[j] = nArray[j+1]
+            nArray[j+1] = temp
+    
+        }
+        
+    }
+    
+}
+console.log(nArray)
