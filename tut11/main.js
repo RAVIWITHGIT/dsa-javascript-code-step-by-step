9a2593ffa6951b24b03d3756912ebf9b5ba0a527
ravi lakshakar <ravilakshakar336@gmail.com>
2023-12-02 16:50:02 +0530

    Recursion Data Structure

---
+++ tut11/main.js
@@ -0,0 +1,39 @@
+// -----------------------Recursion Data Structure-----------------------------------
+
+// if we call function in outer function then this is called recursion data structure
+
+// function myfun(i) {
+//     console.log(i)
+//     if(i<10){
+//         myfun(i+1)
+//     }
+    
+// }
+
+// myfun(1)
+
+
+//********** find factorial in recursion data structure , more about this how this function run please see below code
+
+// function facto(i){
+    
+//     if(i==1){
+//         return 1
+//     }
+//     return i*facto(i-1)
+
+// }
+// console.log(facto(5)) 
+
+
+// --------------------------------------
+// function facto(i){
+    
+//     if(i==1){
+//         return 1
+//     }
+//     return i
+
+// }
+// let a = facto(5)*facto(4)*facto(3)*facto(2)*facto(1)
+// console.log(a)
