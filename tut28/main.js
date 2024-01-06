1dcd0a3dabad14f6e651d3e6df2c7a9f7d815ffc
ravi lakshakar <ravilakshakar336@gmail.com>
2024-01-06 18:19:59 +0530

    how to build logical skills

---
+++ tut28/main.js
@@ -0,0 +1,30 @@
+// ------------------------ How to Build Logical skills ---------------------------------------
+
+/*
+PROBLEM SOLVING AND LOGIC BUILDING
+1. Understand complete Problem.
+2. Find Solution in General Way.
+3. Break the Problem in parts.
+4. think coding Solution.
+5. Re-analyse solution.
+6. Then write code
+
+*/
+
+//Example - Find Even Values from array, Make Double of every element and Find Array length manually
+
+// let arr = [10,7,19,22,8,100,89]
+
+// let narr = []
+// let count=0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i]%2==0){
+//         count++;
+//         narr.push(arr[i]*2)
+//     }
+    
+// }
+// console.log(narr)
+// console.log("the length of this array is ",count)
+
