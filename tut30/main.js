254f7743c9b25054bb6482e9f08e9662fe66d175
ravi lakshakar <ravilakshakar336@gmail.com>
2024-01-10 21:10:37 +0530

    palindrome

---
+++ tut30/main.js
@@ -0,0 +1,19 @@
+// -------------------************** palindrome ******************----------------------------------
+
+// function palindrom(getstr){
+//     let f = 0;
+//     let s = getstr.length-1;
+//     let status = true
+//     while(f<s){
+//         if(getstr[f]!=getstr[s]){
+//             status = false
+//         }
+//         f++
+//         s--
+//     }
+//     return status
+
+
+// }
+
+// console.log(palindrom('lolser'))
