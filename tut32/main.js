6e59165d8286181879b4698e343207d35b0d3531
ravi lakshakar <ravilakshakar336@gmail.com>
2024-01-18 20:56:39 +0530

    Object (Hash Table)

---
+++ tut32/main.js
@@ -0,0 +1,77 @@
+// ----------------------------- Object (Hash Table) ----------------------------------------------
+
+// let data = {
+//     name:"ravi",
+//     age:20,
+//     mail:"ravilak219@gmail.com",
+//     getname:function(){
+//         return this.name
+//     }
+
+
+// }
+// console.log(data)
+
+
+// --------------------------------
+// let data = {
+//     name:"ravi",
+//     age:20,
+//     mail:"ravilak219@gmail.com",
+//     getname:function(){
+//         return this.name
+//     }
+
+
+// }
+// data.contact = 8890831283;
+// console.log(data)
+
+
+// -------------------------------
+// let data = {
+//     name:"ravi",
+//     age:20,
+//     mail:"ravilak219@gmail.com",
+//     getname:function(){
+//         return this.name
+//     }
+
+
+// }
+// delete data.name
+// data.age = 19;
+// console.log(data)
+
+// -----------------------------
+// let data = {
+//     name:"ravi",
+//     age:20,
+//     mail:"ravilak219@gmail.com",
+//     getname:function(){
+//         return this.name
+//     }
+
+
+// }
+
+// console.log(Object.keys(data))
+// console.log(Object.values(data))
+// console.log(Object.entries(data))
+
+
+// -------------------------------
+// let data = {
+//     name:"ravi",
+//     age:20,
+//     mail:"ravilak219@gmail.com",
+//     getname:function(){
+//         return this.name
+//     }
+
+
+// }
+
+// for(let i in data){
+//     console.log(data[i])
+// }
